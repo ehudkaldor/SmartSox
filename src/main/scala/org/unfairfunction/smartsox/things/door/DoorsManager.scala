8bb8cb572024d79feb9f9dccf9c62043c279d7cf
Ehud Kaldor <ehud.kaldor@gmail.com>
2016-10-25 23:55:05 -0700

    implemented Commands, implemented processCommand, added props()

--- src/main/scala/org/unfairfunction/smartsox/things/door/DoorsManager.scala
+++ src/main/scala/org/unfairfunction/smartsox/things/door/DoorsManager.scala
@@ -1,20 +1,30 @@
 package org.unfairfunction.smartsox.things.door
 
 import org.unfairfunction.smartsox.actors.ThingsManager
+import akka.actor.Props
+import org.unfairfunction.smartsox.actors.Thing.{GetState, Die}
+import org.unfairfunction.smartsox.things.door.Door.{OpenDoor, CloseDoor}
 
 object DoorsManager {
   import ThingsManager._
   
+  case class AddDoor(doorId: String) extends Command
+  case class RemoveDoor(doorId: String) extends Command
+  case class Open(doorId: String) extends Command
+  case class Close(doorId: String) extends Command
   
+  def props(id: String) = Props(new DoorsManager)
 }
 
 class DoorsManager extends ThingsManager {
   import DoorsManager._
   
   override def processCommand = {
-    case _ =>
+    case AddDoor(id) => processThingCommand(id, GetState)
+    case RemoveDoor(id) => processThingCommand(id, Die)
+    case Open(id) => processThingCommand(id, OpenDoor)
+    case Close(id) => processThingCommand(id, CloseDoor)    
   }
-  
-  override def thingProps(id: String) = Door.props(id)
-  
+    
+  override def props(id: String) = DoorsManager.props(id)
 }
