8e5b5de8b606e2293cbdbdc993869775555f37e6
Ehud Kaldor <ehud.kaldor@gmail.com>
2016-10-27 23:27:40 -0700

    different persistenceId per test, added another stage to testing opening to capture both DoorOpening and DoorOpened, added closing test

--- src/test/scala/org/unfairfunction/smartsox/things/door/DoorSpec.scala
+++ src/test/scala/org/unfairfunction/smartsox/things/door/DoorSpec.scala
@@ -12,15 +12,21 @@ import org.unfairfunction.smartsox.actors.Thing
 import org.unfairfunction.smartsox.actors.Thing.Uninitialized
 import org.unfairfunction.smartsox.things.door.Door.OpenDoor
 import org.unfairfunction.smartsox.things.door.Door.Opened
+import org.scalatest.BeforeAndAfterEach
+import org.unfairfunction.smartsox.things.door.Door.Opening
+import org.unfairfunction.smartsox.things.door.Door.CloseDoor
+import org.unfairfunction.smartsox.things.door.Door.Closing
+import org.unfairfunction.smartsox.things.door.Door.Closed
 
-class ChatClientSpec(system: ActorSystem)
+class DoorSpec(system: ActorSystem)
   extends TestKit(system)
   with ImplicitSender
   with Matchers
   with FlatSpecLike
-  with BeforeAndAfterAll {
+  with BeforeAndAfterAll
+  with InMemoryCleanup {
 
-  def this() = this(ActorSystem("ChatClientSpec"))  
+  def this() = this(ActorSystem("DoorSpec"))  
   implicit val timeout = Timeout(10 seconds)
 
   implicit val executionContext = system.dispatcher
@@ -30,20 +36,31 @@ class ChatClientSpec(system: ActorSystem)
   }
   
   "Door" should "should be created correctly" in {
-    val door = system.actorOf(Door.props("testdoor"))
+    val door = system.actorOf(Door.props("testdoor1"))
     door should not be null
   }
   
   it should "return state Uninitialized if not changed" in {
-    val door = system.actorOf(Door.props("testdoor"))
+    val door = system.actorOf(Door.props("testdoor2"))
     door ! GetState
     expectMsg(Uninitialized)
   }
   
-  it should "return state Opened after sent message to open door" in {
-    val door = system.actorOf(Door.props("testdoor"))
+  it should "return state Opening after sent message to open door, and Opened afterwards" in {
+    val door = system.actorOf(Door.props("testdoor3"))
     door ! OpenDoor
+    expectMsg(Opening)
+    Thread.sleep(1000)
     door ! GetState
     expectMsg(Opened)
+  }
+  
+  it should "return state Closing after sent message to close door, and Closed afterwards" in {
+    val door = system.actorOf(Door.props("testdoor4"))
+    door ! CloseDoor
+    expectMsg(Closing)
+    Thread.sleep(1000)
+    door ! GetState
+    expectMsg(Closed)
   }
 }
