99e23f8f0f392726dbb402d12f90aa5f844ae6af
Ehud Kaldor <ehud.kaldor@gmail.com>
2016-11-02 23:02:36 -0700

    changed "case abcd" semantics, reusing code, prop
    changed 'case EVENT' to 'case e@EVENT' in Receive, for better
    abstractions
    in processThingCommand, replaced logic with call to findOrCreate
    added (abstract) method to get the Props of the corresponding Thing

--- src/main/scala/org/unfairfunction/smartsox/actors/ThingsManager.scala
+++ src/main/scala/org/unfairfunction/smartsox/actors/ThingsManager.scala
@@ -13,7 +13,10 @@ object ThingsManager {
   trait Event {
     protected val createTime: Calendar = Calendar.getInstance
   }
-
+  
+  case class Retire(id: String) extends Command
+  case class Unretire(id: String) extends Command
+  
   val maxThings = 40
   val thingsToKillAtOnce = 20
   
@@ -29,28 +32,42 @@ trait ThingsManager extends Actor with ActorLogging {
   override def receive = processCommand orElse defaultProcessCommand
 
   private def defaultProcessCommand: Receive = {
-    case Terminated(actor) => log.info(s"actor $actor terminated")
+    case t@Terminated(actor) => {
+      log.info(s"actor $actor terminated. sending to parent ${context.parent.path.name}")
+      context.parent ! t
+    }
+    case ThingsManager.Retire(id) => {
+      processThingCommand(id, Thing.Retire)
+    }
+    case ThingsManager.Unretire(id) => {
+      processThingCommand(id, Thing.Unretire)
+    }
   }
   
   def processThingCommand(thingId: String, command: Thing.Command) = {
-    val maybeChild = context child thingId
-    maybeChild match {
-      case Some(child) =>
-        child forward command
-      case None =>
-        val child = create(thingId)
-        child forward command
-    }
+//    val maybeChild = context child thingId
+//    maybeChild match {
+//      case Some(child) =>
+//        child forward command
+//      case None =>
+//        val child = create(thingId)
+//        child forward command
+//    }
+    val child = findOrCreate(thingId)
+    child forward command
+    
   }
 
   protected def findOrCreate(id: String): ActorRef =
     context child id getOrElse create(id)
 
   protected def create(id: String): ActorRef = {
-    val nextThing = context.actorOf(props(id), id)
+    val nextThing = context.actorOf(thingProps(id), id)
     context watch nextThing
     nextThing
   }
+  
+  def thingProps(id: String): Props
   
   def props(id: String): Props
 }
