edac1a3220f07cd2a73c485a8c4369b0d6a4db62
Ehud Kaldor <ehud.kaldor@gmail.com>
2016-11-02 23:04:00 -0700

    moved adterAll() here, as it is common

--- src/test/scala/org/unfairfunction/smartsox/things/door/InMemoryCleanup.scala
+++ src/test/scala/org/unfairfunction/smartsox/things/door/InMemoryCleanup.scala
@@ -5,10 +5,17 @@ import org.scalatest.Suite
 import akka.actor.ActorSystem
 import akka.testkit.TestProbe
 import akka.persistence.inmemory.extension.{ InMemoryJournalStorage, InMemorySnapshotStorage, StorageExtension }
+import org.scalatest.BeforeAndAfterAll
+import akka.testkit.TestKit
 
-trait InMemoryCleanup extends BeforeAndAfterEach { _: Suite =>
+trait InMemoryCleanup extends BeforeAndAfterEach with BeforeAndAfterAll { _: Suite =>
 
   implicit def system: ActorSystem
+  
+  override def afterAll = {
+    system.terminate()
+  }
+
 
   override protected def beforeEach(): Unit = {
     val tp = TestProbe()
