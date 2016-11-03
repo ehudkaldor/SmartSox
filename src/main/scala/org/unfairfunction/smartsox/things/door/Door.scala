ba7fe9550abed9156762409f9299f1163188d392
Ehud Kaldor <ehud.kaldor@gmail.com>
2016-11-02 22:59:18 -0700

    locking, default case for Receive and case semantics
    added state, commands, events and handlers for locking, unlocking and
    locked
    added respond() to all default cases of all Receive, so generalities on
    received Command/Event will get a reponse of State
    changed case semantics in Receive, where possible, from "case EVENT" to
    "case e@EVENT" to allow abstractions

--- src/main/scala/org/unfairfunction/smartsox/things/door/Door.scala
+++ src/main/scala/org/unfairfunction/smartsox/things/door/Door.scala
@@ -12,18 +12,29 @@ object Door {
   
   case object Opened extends State
   case object Closed extends State
+  case object Locked extends State
   case object Opening extends State
   case object Closing extends State
+  case object Locking extends State
+  case object Unlocking extends State
   
   case object OpenDoor extends Command
   case object CloseDoor extends Command
+  case object LockDoor extends Command
+  case object UnlockDoor extends Command
   
   case object DoorOpening extends Event
   case object DoorClosing extends Event
   case object DoorOpened extends Event
   case object DoorClosed extends Event
+  case object DoorLocking extends Event
+  case object DoorUnlocking extends Event
+  case object DoorLocked extends Event
+  
   case class DoorClosingFailed(reason: String) extends CommandFailed
   case class DoorOpeningFailed(reason: String) extends CommandFailed
+  case class DoorLockingFailed(reason: String) extends CommandFailed
+  case class DoorUnlockingFailed(reason: String) extends CommandFailed
   
   
   def props(id: String): Props = Props(new Door(id))
@@ -47,6 +58,9 @@ class Door(val persistenceId: String) extends Thing {
       case Closed => context become closed
       case Opening => context become opening
       case Closing => context become closing
+      case Locked => context become locked
+      case Locking => context become locking
+      case Unlocking => context become unlocking
       case _ => context become initial
     }
   }
@@ -69,12 +83,24 @@ class Door(val persistenceId: String) extends Thing {
         context become opening
         state = Opening
       }
+      case DoorLocking => {
+        context become locking
+        state = Locking
+      }
+      case DoorUnlocking => {
+        context become unlocking
+        state = Unlocking
+      }
+      case DoorLocked => {
+        context become locked
+        state = Locked
+      }
       case _ => {
         context become initial
         state = Uninitialized
       }
     }
-    log.debug(s"state updated to $state")
+    log.debug(s"door $persistenceId, state updated to $state")
   }
   
   val initial: Receive = {
@@ -106,6 +132,7 @@ class Door(val persistenceId: String) extends Thing {
     }
     case evt => {
 //      log.debug(s"door $persistenceId, state $state, received event $evt and not sure what to do. ignoring")
+      respond()
     }
   }
   
@@ -129,6 +156,7 @@ class Door(val persistenceId: String) extends Thing {
     }
     case evt => {
 //      log.debug(s"door $persistenceId, state $state, received event $evt and not sure what to do. ignoring")
+      respond()
     }
   }
   
@@ -137,14 +165,19 @@ class Door(val persistenceId: String) extends Thing {
       log.debug(s"door $persistenceId, state $state, received GetState")
       respond()
     }
-    case OpenDoor => {
-      log.debug(s"door $persistenceId, state $state, received OpenDoor")
+    case e@OpenDoor => {
+      log.debug(s"door $persistenceId, state $state, received $e")
       persist(DoorOpening)(afterEventPersisted)
       openDoor 
     }
-    case DoorOpening => {
-      log.debug(s"got event DoorOpening in state $state, meaning some opened the door manually")
-      persist(DoorOpening)(afterEventPersisted)
+    case e@DoorOpening => {
+      log.debug(s"got event $e in state $state, meaning someone opened the door manually")
+      persist(e)(afterEventPersisted)
+    }
+    case e@LockDoor => {
+      log.debug(s"door $persistenceId, state $state, received $e")
+      persist(DoorLocking)(afterEventPersisted)
+      lockDoor     
     }
     case Die => {
       log.debug(s"door $persistenceId, got command to Die. Dying")
@@ -152,9 +185,30 @@ class Door(val persistenceId: String) extends Thing {
     }
     case evt => {
 //      log.debug(s"door $persistenceId, state $state, received event $evt and not sure what to do. ignoring")
+      respond()
     }
   }
   
+  val locked: Receive = {
+    case GetState => {
+      log.debug(s"door $persistenceId, state $state, received GetState")
+      respond()
+    }
+    case e@UnlockDoor => {
+      log.debug(s"door $persistenceId, state $state, received $e")
+      persist(DoorUnlocking)(afterEventPersisted)
+      unlockDoor
+    }
+    case Die => {
+      log.debug(s"door $persistenceId, got command to Die. Dying")
+      context stop self
+    }
+    case evt => {
+//      log.debug(s"door $persistenceId, state $state, received event $evt and not sure what to do. ignoring")
+      respond()
+    }
+  }
+
   val opening: Receive = {
     case GetState => {
       log.debug(s"door $persistenceId, state $state, received GetState")
@@ -174,6 +228,7 @@ class Door(val persistenceId: String) extends Thing {
     }
     case evt => {
 //      log.debug(s"door $persistenceId, state $state, received event $evt and not sure what to do. ignoring")
+      respond()
     }
   }
   
@@ -196,9 +251,56 @@ class Door(val persistenceId: String) extends Thing {
     }
     case evt => {
 //      log.debug(s"door $persistenceId, state $state, received event $evt and not sure what to do. ignoring")
+      respond()
     }
   }
   
+  val locking: Receive = {
+    case GetState => {
+      log.debug(s"door $persistenceId, state $state, received GetState")
+      respond()
+    }
+    case e@DoorLocked => {
+      log.debug(s"door $persistenceId, state $state, received DoorLocked")
+      persist(e)(afterEventPersisted)
+    }
+    case e@DoorLockingFailed(msg) => {
+      log.error(s"door $persistenceId, state $state, door failed to lock with message: $msg")
+      persist(e)(afterEventPersisted)
+    }
+    case Die => {
+      log.debug(s"door $persistenceId, got command to Die. Dying")
+      context stop self
+    }
+    case evt => {
+//      log.debug(s"door $persistenceId, state $state, received event $evt and not sure what to do. ignoring")
+      respond()
+    }
+  }
+
+  val unlocking: Receive = {
+    case e@GetState => {
+      log.debug(s"door $persistenceId, state $state, received $e")
+      respond()
+    }
+    case e@DoorClosed => {
+      log.debug(s"door $persistenceId, state $state, received $e")
+      persist(e)(afterEventPersisted)
+    }
+    case e@DoorUnlockingFailed(msg) => {
+      log.error(s"door $persistenceId, state $state, door failed to unlock with message: $msg")
+      persist(e)(afterEventPersisted)
+    }
+    case Die => {
+      log.debug(s"door $persistenceId, got command to Die. Dying")
+      context stop self
+    }
+    case evt => {
+//      log.debug(s"door $persistenceId, state $state, received event $evt and not sure what to do. ignoring")
+      respond()
+    }
+  }
+
   override val receiveCommand: Receive = initial
   
   private def closeDoor = {
@@ -209,5 +311,15 @@ class Door(val persistenceId: String) extends Thing {
   private def openDoor = {
     log.debug(s"door $persistenceId, opening door")
     self ! DoorOpened
+  }
+  
+  private def lockDoor = {
+    log.debug(s"door $persistenceId, locking door")
+    self ! DoorLocked
+  }
+  
+  private def unlockDoor = {
+    log.debug(s"door $persistenceId, unlocking door")
+    self ! DoorClosed
   }
 }
