c77a6ab55983c30ffd83a108cdc794b29929437f
Ehud Kaldor <ehud.kaldor@gmail.com>
2016-12-28 12:20:49 -0800

    added, to be used (future) for HTTPS support by akka-http

---
+++ src/main/scala/org/unfairfunction/smartsox/api/HttpsConfig.scala
@@ -0,0 +1,28 @@
+package org.unfairfunction.smartsox.api
+
+import java.io.InputStream
+import java.security.{ SecureRandom, KeyStore }
+import javax.net.ssl.{ SSLContext, TrustManagerFactory, KeyManagerFactory }
+
+import akka.http.scaladsl.{ ConnectionContext, HttpsConnectionContext, Http }
+//import com.typesafe.sslconfig.akka.AkkaSSLConfig
+
+class HttpsConfig {
+  val password: Array[Char] = "change me".toCharArray // do not store passwords in code, read them from somewhere safe!
+
+  val ks: KeyStore = KeyStore.getInstance("PKCS12")
+  val keystore: InputStream = getClass.getClassLoader.getResourceAsStream("server.p12")
+
+  require(keystore != null, "Keystore required!")
+  ks.load(keystore, password)
+
+  val keyManagerFactory: KeyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+  keyManagerFactory.init(ks, password)
+ 
+  val tmf: TrustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+  tmf.init(ks)
+
+  val sslContext: SSLContext = SSLContext.getInstance("TLS")
+  sslContext.init(keyManagerFactory.getKeyManagers, tmf.getTrustManagers, new SecureRandom)
+  val https: HttpsConnectionContext = ConnectionContext.https(sslContext)
+}
